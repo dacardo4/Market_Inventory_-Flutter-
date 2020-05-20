776ce35d68f3a5789247b419984197d959788a51
prueba <42043332+dacardo4@users.noreply.github.com>
2020-05-19 21:26:50 -0500

    Update HomePage

--- lib/src/pages/homa_page.dart
+++ lib/src/pages/homa_page.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:market_inventory/src/providers/quantity_provider.dart';
 import 'package:market_inventory/src/widgets/product-card.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ListView(
-        padding: EdgeInsets.all(10.0),
-        children: <Widget>[
-          SizedBox( height: 20.0,),
-          ProductCard(),
-        ],
-      ),
+      child: _myList(),
     );
+  }
+
+  Widget _myList() {
+    return FutureBuilder(
+      //future: productProvider.getData(),
+      future: quantityProvider.getData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _itemsList(snapshot.data, context),
+        );
+      },
+    );
+  }
+
+  List<Widget> _itemsList( List<dynamic> data, BuildContext context ) {
+    final List<Widget> opciones = [];
+    data.forEach( (opt) {
+      final widgetTemp = ProductCard(
+        nameProduct: opt['product']['productName'],
+        quantityStockProduct: opt['quantityInStock'],
+        quantityBuyProduct: opt['quantityToBuy']
+      );
+      opciones..add(widgetTemp)
+              ..add(Divider());
+    });
+    return opciones;
   }
 }
