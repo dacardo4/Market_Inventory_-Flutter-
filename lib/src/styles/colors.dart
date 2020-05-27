1747a9d308f67c429dc251dcfe2587ad83f99036
prueba <42043332+dacardo4@users.noreply.github.com>
2020-05-27 11:38:21 -0500

    update colors file

--- lib/src/styles/colors.dart
+++ lib/src/styles/colors.dart
@@ -1,6 +1,7 @@
-
-int textTitle = 0xff1e96fc;
-int textSubtitle = 0xffffa9e7;
-int textGeneral = 0xff473bf0;
-int backgroundApp = 0xffff84e8;
-int backgroundCard = 0xff414361;
+class Colors {
+  static const int textTitle = 0xff1e96fc;
+  static const int textSubtitle = 0xffffa9e7;
+  static const int textGeneral = 0xff473bf0;
+  static const int backgroundApp = 0xffff84e8;
+  static const int backgroundCard = 0xff414361;
+}
