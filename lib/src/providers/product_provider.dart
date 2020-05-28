9b03eede467688fcdebecbbedbda03464397929c
prueba <42043332+dacardo4@users.noreply.github.com>
2020-05-27 21:51:04 -0500

    better programing practics on providers

--- lib/src/providers/product_provider.dart
+++ lib/src/providers/product_provider.dart
@@ -1,21 +1,24 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+
 import 'package:market_inventory/src/providers/quantity_provider.dart';
 import 'package:market_inventory/src/utils/constants.dart';
 
 class _ProductProvider {
+  static const String localUrl = Constants.urlBack+'products';
   List<dynamic> data = [];
   Map<String, dynamic> postData;
 
   Future<List<dynamic>> getAllProducts() async {
-    final answer = await http.get(Constants.urlBack+'products');
+    final answer = await http.get(localUrl);
     if (answer.statusCode == 200) data = json.decode(answer.body);
     else  data = []; //print('Status Error no 200');
     return data;
   }
 
   Future<Map<String, dynamic>> postProducts(String productName, int idUser, int idCategory, int idSubcategory, int quantityInStock, int quantityToBuy) async {
-    final answer = await http.post(Constants.urlBack+'products',
+    final answer = await http.post(
+      localUrl,
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
--- lib/src/providers/quantity_provider.dart
+++ lib/src/providers/quantity_provider.dart
@@ -1,29 +1,32 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:market_inventory/src/utils/constants.dart';
 
 class _QuantityProvider {
+  static const String localUrl = Constants.urlBack+'quantities';
   List<dynamic> data = [];
   Map<String, dynamic> postData;
 
   _QuantityProvider();
 
   Future<List<dynamic>> getAllQuantityData() async {
-    final answer = await http.get(Constants.urlBack+'quantities'+'?filter={"include":"product"}');
+    final answer = await http.get(localUrl+'?filter={"include":"product"}');
     if (answer.statusCode == 200) data = json.decode(answer.body);
     else  data = []; //print('Status Error no 200');
     return data;
   }
 
   Future<List<dynamic>> getQuantityShoppingList() async {
-    final answer = await http.get(Constants.urlBack+'quantities/shoppingList');
+    final answer = await http.get(localUrl+'/shoppingList');
     if (answer.statusCode == 200) data = json.decode(answer.body);
     else  data = []; //print('Status Error no 200');
     return data;
   }
 
   Future<Map<String, dynamic>> postQuantityData(int idProduct, int quantityInStock, int quantityToBuy, int idUser) async {
-    final answer = await http.post(Constants.urlBack+'quantities',
+    final answer = await http.post(
+      localUrl,
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -40,3 +43,54 @@ class _QuantityProvider {
   }
 }
 final quantityProvider = new _QuantityProvider();
+
+// Widget myAlert() {
+//   return AlertDialog(
+//     title: Text('AlertDialog Title'),
+//     content: SingleChildScrollView(
+//       child: ListBody(
+//         children: <Widget>[
+//           Text('This is a demo alert dialog.'),
+//           Text('Would you like to approve of this message?'),
+//         ],
+//       ),
+//     ),
+//     actions: <Widget>[
+//       FlatButton(
+//         child: Text('Approve'),
+//         onPressed: () {
+//           Navigator.of(context).pop();
+//         },
+//       ),
+//     ],
+//   );
+// }
+
+
+// Future<void> _showMyDialog() async {
+//   return showDialog<void>(
+//     context: context,
+//     barrierDismissible: false, // user must tap button!
+//     builder: (BuildContext context) {
+//       return AlertDialog(
+//         title: Text('AlertDialog Title'),
+//         content: SingleChildScrollView(
+//           child: ListBody(
+//             children: <Widget>[
+//               Text('This is a demo alert dialog.'),
+//               Text('Would you like to approve of this message?'),
+//             ],
+//           ),
+//         ),
+//         actions: <Widget>[
+//           FlatButton(
+//             child: Text('Approve'),
+//             onPressed: () {
+//               Navigator.of(context).pop();
+//             },
+//           ),
+//         ],
+//       );
+//     },
+//   );
+// }
